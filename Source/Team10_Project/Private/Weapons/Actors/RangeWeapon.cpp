b267388f3897fa2d4cd25ea77f572bbc96f3935f
LimHyeonGyun <200hk@naver.com>
2025-08-02 02:02:56 +0900

    Add.RangeWeapon
    RangeWeapon 추가
    1.단발 사격 기능 구현
    2.사격모드 스위칭기능 구현

---
+++ Source/Team10_Project/Private/Weapons/Actors/RangeWeapon.cpp
@@ -0,0 +1,129 @@
+#include "Weapons/Actors/RangeWeapon.h"
+#include "Weapons/Actors/ProjectileBase.h"
+
+ARangeWeapon::ARangeWeapon()
+	:LeverType(ERangeLeverType::Single), FireType(ERangeFireType::SingleShot),
+	FireSpeed(0), MaxBulletAmount(0), CurBulletAmount(0), bIsFire(true)
+{
+	WeaponType = EWeaponType::Range;
+
+	ProjectilePoint = WeaponMesh->GetSocketLocation(TEXT("MuzzleSocket"));
+}
+
+void ARangeWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	if (LeverType == ERangeLeverType::Single)
+	{
+		FireType = ERangeFireType::SingleShot;
+	}
+	if (LeverType == ERangeLeverType::Point)
+	{
+		FireType = ERangeFireType::PointFire;
+	}
+	if (LeverType == ERangeLeverType::Repeatedly)
+	{
+		FireType = ERangeFireType::Repeatedly;
+	}
+}
+void ARangeWeapon::Attack(AActor* Activator)
+{
+	Super::Attack(Activator);
+
+	if (bIsFire && FireState == ERangeFireState::Load)
+	{
+		FireState = ERangeFireState::Fire;
+
+		AProjectileBase* Projectile = CreateDefaultSubobject<AProjectileBase>(TEXT("AProjectileBase")); //ObjectPool된 총알로 초기화 시키기
+		Projectile->Activate(this, ProjectilePoint);
+		GetWorld()->GetTimerManager().SetTimer(
+			FireTimerHandle,
+			this,
+			&ARangeWeapon::SetFireState,
+			RateOfFire,
+			false
+		);
+
+		CurBulletAmount--;
+		//장전된 총알이 다 떨어졌으면
+		if (CurBulletAmount == 0)
+		{
+			bIsFire = false;
+			FireState = ERangeFireState::Idle;
+		}
+	}
+}
+
+
+//class AIneventory;
+
+void ARangeWeapon::Reload(AActor* Activator)
+{
+	if (!Activator) return;
+
+	FireState = ERangeFireState::Reload;
+	/*AIneventory* Inven = CreateDefaultSubobject<AIneventory>(TEXT("AIneventory"));
+	if (Inven = Cast<AIneventory>(Activator))
+	{
+		int32 RemainingBullet = Inven->GetRaminingBullet();
+		if (RemainingBullet > 0 && MaxBulletAmount > CurBulletAmount)
+		{
+			int32 NeedBullet = MaxBulletAmount - CurBulletAmount;
+			if (RemainingBullet >= NeedBullet)
+			{
+				CurBulletAmount += NeedBullet;
+				Inven->SetRemainingBullet(-NeedBullet);
+			}
+			else if (RemainingBullet < NeedBullet)
+			{
+				CurBulletAmount += RemainingBullet;
+				Inven->SetRemainingBullet(-RemainingBullet);
+			}
+			bIsFire = true;
+			FireState = ERangeFireState::Load;
+		}
+	}*/
+}
+
+ERangeLeverType ARangeWeapon::GetRangeLeverType()
+{
+	return LeverType;
+}
+
+ERangeFireType ARangeWeapon::GetAttackType()
+{
+	return FireType;
+}
+
+float ARangeWeapon::GetFireSpeed()
+{
+	return FireSpeed;
+}
+
+void ARangeWeapon::SetFireState()
+{
+	FireState = ERangeFireState::Load;
+}
+
+void ARangeWeapon::SwitchFireType()
+{
+	if (FireType == ERangeFireType::SingleShot)
+	{
+		if (LeverType == ERangeLeverType::Point)
+		{
+			FireType = ERangeFireType::PointFire;
+		}
+		if (LeverType == ERangeLeverType::Repeatedly)
+		{
+			FireType = ERangeFireType::Repeatedly;
+		}
+	}
+	else if (FireType == ERangeFireType::PointFire)
+	{
+		FireType = ERangeFireType::SingleShot;
+	}
+	else if (FireType == ERangeFireType::Repeatedly)
+	{
+		FireType = ERangeFireType::SingleShot;
+	}
+}
---
+++ Source/Team10_Project/Public/Weapons/Actors/RangeWeapon.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/Actors/WeaponBase.h"
+#include "RangeWeapon.generated.h"
+
+UCLASS()
+class TEAM10_PROJECT_API ARangeWeapon : public AWeaponBase
+{
+	GENERATED_BODY()
+
+public:
+	ARangeWeapon();
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|FireData")
+	ERangeLeverType LeverType;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|FireData")
+	ERangeFireType FireType;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|FireData")
+	float FireSpeed;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|FireData")
+	int32 MaxBulletAmount;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|FireData")
+	int32 CurBulletAmount;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|FireData")
+	bool bIsFire;
+
+	ERangeLeverType GetRangeLeverType();
+	ERangeFireType GetAttackType();
+
+	FTimerHandle FireTimerHandle;
+	FTimerDelegate TimerDel;
+public:
+	virtual void Attack(AActor* Activator) override;
+
+	void Reload(AActor* Activator);
+	float GetFireSpeed();
+	void SetFireState();
+	void SwitchFireType();
+};
