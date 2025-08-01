00153a58edc39916221767c948adf3d960007205
LimHyeonGyun <200hk@naver.com>
2025-08-02 02:04:55 +0900

    Add
    Bullet 구현

---
+++ Source/Team10_Project/Private/Weapons/Actors/Bullet.cpp
@@ -0,0 +1,8 @@
+#include "Weapons/Actors/Bullet.h"
+
+ABullet::ABullet()
+{
+	ProjectileSpeed = 850.0f;
+	ProjectileRange = 800.0f;
+	Power = 10;
+}
---
+++ Source/Team10_Project/Public/Weapons/Actors/Bullet.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/Actors/ProjectileBase.h"
+#include "Bullet.generated.h"
+
+UCLASS()
+class TEAM10_PROJECT_API ABullet : public AProjectileBase
+{
+	GENERATED_BODY()
+	
+	ABullet();
+};
