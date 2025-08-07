32852c594dcf75bfc257fae73cacbbf35e290d31
LimHyeonGyun <200hk@naver.com>
2025-08-08 05:07:51 +0900

    update: ProjectileBase.h
    Projectile에 필요한 위치와 회전정보 값을 받고 이동에 필요한 MoveComponent추가

--- Source/Team10_Project/Public/Weapons/Actors/ProjectileBase.h
+++ Source/Team10_Project/Public/Weapons/Actors/ProjectileBase.h
@@ -6,6 +6,7 @@
 #include "ProjectileBase.generated.h"
 
 class ARangeWeapon;
+class UProjectileMovementComponent;
 
 UCLASS()
 class TEAM10_PROJECT_API AProjectileBase : public AWeaponBase, public IPoolObjectInterface
@@ -13,28 +14,47 @@ class TEAM10_PROJECT_API AProjectileBase : public AWeaponBase, public IPoolObjec
 	GENERATED_BODY()
 public:
 	AProjectileBase();
-
+	virtual void Tick(float Time)override;
 private:
 	FTimerHandle ProjectileTimerHandle;
+	int32 WeaponPower;
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile|FireData")
+	bool bIsActive;
 
 protected:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|FireData")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile|FireData")
+	UProjectileMovementComponent* ProjectileMovementComp;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile|FireData")
+	UBoxComponent* ProjectileCollision;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile|FireData")
+	FVector ProjectileLocation;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile|FireData")
+	FRotator ProjectileRotation;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile|FireData")
+	FVector ProjectileDir;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile|FireData")
 	float ProjectileSpeed;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|FireData")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile|FireData")
 	float ProjectileRange;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|FireData")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile|FireData")
 	int32 TotalDamage;
 
 	float LifeTime;
-public:
-	int32 WeaponPower;
+	bool Only;
 
 public:
-	void OnHit(AActor* OtherActor);
-	void Activate(ARangeWeapon* ActiveWeapon, FVector BulletPoint, FRotator BulletRotation);
+	virtual void OnHit(UPrimitiveComponent* HitComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse,
+		const FHitResult& Hit) override;
+	void Activate(ARangeWeapon* ActiveWeapon, FVector BulletPoint, FRotator BulletRotation, FVector FireDir);
 	void SetDamage(int32 WPower);
-	void ProjectileLifeTime(ARangeWeapon* ActiveWeapon);
+	void ProjectileLifeTime();
+	virtual void ProjectileMovement();
 
+	virtual void SetActive_Implementation(bool Active) override;
 	virtual bool GetIsActive_Implementation() override;
 	virtual void ActiveObject_Implementation() override;
 	virtual void DeActiveObject_Implementation() override;
