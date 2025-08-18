3d8f431372a4318c483d52848acef77ea68f0c35
gwakgh <gwakggh304@gmail.com>
2025-08-18 23:59:57 +0900

    feat: Gamemode, Spawner, Wave 수정

--- Source/Team10_Project/Private/SpartaSpawnManager.cpp
+++ Source/Team10_Project/Private/SpartaSpawnManager.cpp
@@ -16,6 +16,7 @@ void ASpartaSpawnManager::BeginPlay()
 
 void ASpartaSpawnManager::StartSpawning(const FWaveData& WaveData)
 {
+    UE_LOG(LogTemp, Warning, TEXT("Starting spawning enemies for wave..."));
     PendingSpawnClasses.Empty();
     PendingSpawnCounts.Empty();
     CurrentSpawnIndex = 0;
@@ -26,6 +27,8 @@ void ASpartaSpawnManager::StartSpawning(const FWaveData& WaveData)
         {
             PendingSpawnClasses.Add(Info.EnemyClass);
             PendingSpawnCounts.Add(Info.SpawnCount);
+            UE_LOG(LogTemp, Warning, TEXT("Enemy Class: %s, Count: %d"),
+                *Info.EnemyClass->GetName(), Info.SpawnCount);
         }
     }
 
@@ -40,15 +43,19 @@ void ASpartaSpawnManager::StartSpawning(const FWaveData& WaveData)
 
 void ASpartaSpawnManager::SpawnNextEnemy()
 {
-    if (CurrentSpawnIndex >= PendingSpawnClasses.Num())
+        if (CurrentSpawnIndex >= PendingSpawnClasses.Num())
     {
         GetWorld()->GetTimerManager().ClearTimer(SpawnTimerHandle);
         return;
     }
-
+    
     TSubclassOf<APawn> EnemyClass = PendingSpawnClasses[CurrentSpawnIndex];
     int32& RemainingCount = PendingSpawnCounts[CurrentSpawnIndex];
-
+    
+    UE_LOG(LogTemp, Error, TEXT("Spawning enemy of class: %s, Remaining Count: %d"),
+        *EnemyClass->GetName(), RemainingCount);
+    UE_LOG(LogTemp, Warning, TEXT("index %d"), CurrentSpawnIndex);
+    
     if (EnemyClass && RemainingCount > 0 && SpawnPoints.Num() > 0)
     {
         // 랜덤 스폰 위치
@@ -76,10 +83,15 @@ void ASpartaSpawnManager::SpawnNextEnemy()
             RemainingCount--;
         }
     }
-
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("No valid enemy class or remaining count for index %d"), CurrentSpawnIndex);
+    }
+    
     // 다음 적으로 넘어갈 준비
     if (RemainingCount <= 0)
     {
         CurrentSpawnIndex++;
     }
+    
 }
